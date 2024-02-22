1cde29456a4038101cff9f80c3d190a865f4b95e
Sriram Senthil <sriram.senthil153@gmail.com>
2024-02-22 16:28:36 -0500

    Typescript solution

--- solution.js
+++ solution.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
  
- var twoSum = function(nums, target) {
+ var twoSumJs = function(nums, target) {
     let map = new Map()
     for(let i=0; i<nums.length; i++){
         if (map.has(target - nums[i]))
